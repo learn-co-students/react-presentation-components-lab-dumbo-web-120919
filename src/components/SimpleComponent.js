39948431d616260878635641fc56088999829267
itslizkim <elizabeth.kim93@gmail.com>
2020-02-27 09:59:25 -0500

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,22 @@
 // Code SimpleComponent Here
+import React, { Component } from 'react'
+
+export default class SimpleComponent extends Component {
+
+    state = {
+        mood: "happy"
+    }
+
+    handleToggle = () => {
+        const toggleMood = this.state.mood === "happy" ? "sad" : "happy"
+        this.setState({ mood: toggleMood })
+    }
+
+    render() {
+        return (
+            <div onClick={this.handleToggle}>
+                {this.state.mood}
+            </div>
+        )
+    }
+}
